ccd49a0ef1adddb85e680ca30bd0ad9b25c4e8c5
rbsgaridan <rbsgaridan@usm.edu.ph>
2022-03-21 15:35:41 +0800

    Initital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui';
+  apiPath = 'https://localhost:5001/api/Users';
+  list: any;
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+    this._http.get(this.apiPath)
+      .subscribe(values => {
+        this.list = values
+      });
+  }
+
+
 }
